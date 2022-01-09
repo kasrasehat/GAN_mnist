1b34ad0894e05cd046dd8d1ec236c0d5f0b56836
kasra sehat <kasra.sehat@yahoo.com>
2022-01-09 21:09:16 +0330

    add get_gen_loss function

--- main.py
+++ main.py
@@ -98,3 +98,32 @@ def get_disc_loss(gen, disc, criterion, real, num_images, z_dim, device):
     return disc_loss
 
 
+def get_gen_loss(gen, disc, criterion, num_images, z_dim, device):
+    '''
+    Return the loss of the generator given inputs.
+    Parameters:
+        gen: the generator model, which returns an image given z-dimensional noise
+        disc: the discriminator model, which returns a single-dimensional prediction of real/fake
+        criterion: the loss function, which should be used to compare
+               the discriminator's predictions to the ground truth reality of the images
+               (e.g. fake = 0, real = 1)
+        num_images: the number of images the generator should produce,
+                which is also the length of the real images
+        z_dim: the dimension of the noise vector, a scalar
+        device: the device type
+    Returns:
+        gen_loss: a torch scalar loss value for the current batch
+    '''
+    #     These are the steps you will need to complete:
+    #       1) Create noise vectors and generate a batch of fake images.
+    #           Remember to pass the device argument to the get_noise function.
+    #       2) Get the discriminator's prediction of the fake image.
+    #       3) Calculate the generator's loss. Remember the generator wants
+    #          the discriminator to think that its fake images are real
+
+    noise_vec = get_noise(num_images, z_dim, device = device)
+    fake_images = gen(noise_vec)
+    pred_of_fake = disc(fake_images)
+    label_of_fake = torch.ones_like(pred_of_fake)
+    gen_loss = criterion(pred_of_fake, label_of_fake)
+    return gen_loss
